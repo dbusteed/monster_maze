6d8fa0f64c6a42e2c679d2265fdf185998cef230
dbusteed <busteed.davis@gmail.com>
2022-01-16 20:03:34 -0500

    difficulty

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -7,26 +7,26 @@ var tile_size = 8
 
 # [max_monsters, spawn_time, total_time]
 var _levels = [
-	[2, 3, 5],
-	[3, 3, 10],
-	[4, 3, 20],
-	[5, 3, 20],
+    [2, 3, 5],
+    [3, 3, 10],
+    [4, 3, 20],
+    [5, 3, 20],
 ]
 var levels = {}
 
 func _ready():
-	randomize()
-	
-	# create the levels dictionary. this is done dynamically so 
-	# that you can edit the _levels array without making sure
-	# the keys match up
-	for i in range(0, _levels.size()):
-		levels[i+1] = _levels[i]
-	max_level = _levels.size()
+    randomize()
+    
+    # create the levels dictionary. this is done dynamically so 
+    # that you can edit the _levels array without making sure
+    # the keys match up
+    for i in range(0, _levels.size()):
+        levels[i+1] = _levels[i]
+    max_level = _levels.size()
 
 
 func get_level_data():
-	if (!level in levels):
-		return []
-	else:
-		return levels[level]
+    if (!level in levels):
+        return []
+    else:
+        return levels[level]
--- Scripts/Monster.gd
+++ Scripts/Monster.gd
@@ -5,24 +5,25 @@ onready var player = get_node("/root/World/Player")
 
 var dir = null
 var path = null
+var speed = 20
 var target = null
 var next_step = null
 var velocity = Vector2.ZERO
 
 
 func _physics_process(delta):
-	if Global.playing:
-		path = nav.get_simple_path(global_position, player.global_position, false)
-		if path.size() > 2:
-			next_step = path[2]
-			dir = global_position.direction_to(next_step)
-			velocity = velocity.move_toward(dir * 25, 200 * delta)
-		
-		if $SoftCollision.is_colliding():
-			velocity += $SoftCollision.get_push_vector() * delta * 100
-		
-		velocity = move_and_slide(velocity)
+    if Global.playing:
+        path = nav.get_simple_path(global_position, player.global_position, false)
+        if path.size() > 2:
+            next_step = path[2]
+            dir = global_position.direction_to(next_step)
+            velocity = velocity.move_toward(dir * speed, 200 * delta)
+        
+        if $SoftCollision.is_colliding():
+            velocity += $SoftCollision.get_push_vector() * delta * 100
+        
+        velocity = move_and_slide(velocity)
 
 
 func blink():
-	$AnimationPlayer.play("Blink")
+    $AnimationPlayer.play("Blink")
